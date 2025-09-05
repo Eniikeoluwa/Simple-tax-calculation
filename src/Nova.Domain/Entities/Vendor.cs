6022c63ddd8aeedb6a55ee72a5ccfb23bc7e11e6
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-09-05 21:27:13 +0100

    vendors creation and tenant

--- src/Nova.Domain/Entities/Vendor.cs
+++ src/Nova.Domain/Entities/Vendor.cs
@@ -15,8 +15,10 @@ public class Vendor : BaseEntity
     public decimal WhtRate { get; set; } = 2.0m;
     public bool IsActive { get; set; }
     public string BankId { get; set; }
+    public string TenantId { get; set; }
 
     public virtual Bank Bank { get; set; }
+    public virtual Tenant Tenant { get; set; }
     public virtual ICollection<Payment> Payments { get; set; }
 
     public bool HasVat => TaxType == "VAT" || TaxType == "Both";
