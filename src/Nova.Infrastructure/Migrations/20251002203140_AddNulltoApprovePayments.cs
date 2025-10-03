1b867bab525afb866cc923c78229f08076c541a9
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-10-03 12:53:39 +0100

    payment fixes

---
+++ src/Nova.Infrastructure/Migrations/20251002203140_AddNulltoApprovePayments.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nova.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNulltoApprovePayments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApprovedByUserId",
+                table: "Payments",
+                type: "character varying(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ApprovedByUserId",
+                table: "Payments",
+                type: "character varying(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldNullable: true);
+        }
+    }
+}
