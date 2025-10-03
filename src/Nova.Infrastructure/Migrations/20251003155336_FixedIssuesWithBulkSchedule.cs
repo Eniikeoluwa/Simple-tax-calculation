c3205bf1ee7843b7302291d2afe463c3a52a6ea0
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-10-03 18:40:45 +0100

    finished payments

---
+++ src/Nova.Infrastructure/Migrations/20251003155336_FixedIssuesWithBulkSchedule.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nova.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedIssuesWithBulkSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BulkSchedules_Users_ProcessedByUserId",
+                table: "BulkSchedules");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcessedByUserId",
+                table: "BulkSchedules",
+                type: "character varying(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BulkSchedules_Users_ProcessedByUserId",
+                table: "BulkSchedules",
+                column: "ProcessedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BulkSchedules_Users_ProcessedByUserId",
+                table: "BulkSchedules");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProcessedByUserId",
+                table: "BulkSchedules",
+                type: "character varying(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BulkSchedules_Users_ProcessedByUserId",
+                table: "BulkSchedules",
+                column: "ProcessedByUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
