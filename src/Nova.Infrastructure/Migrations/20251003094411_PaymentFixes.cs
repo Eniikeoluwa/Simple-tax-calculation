9bacb3dfd9c70d9e0440da8d1e58094c3c365221
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-10-03 12:53:46 +0100

    payment fixes

---
+++ src/Nova.Infrastructure/Migrations/20251003094411_PaymentFixes.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nova.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentFixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Tenants_TenantId",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                table: "Payments",
+                type: "character varying(50)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParentPaymentId",
+                table: "Payments",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Tenants_TenantId",
+                table: "Payments",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Tenants_TenantId",
+                table: "Payments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                table: "Payments",
+                type: "character varying(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ParentPaymentId",
+                table: "Payments",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Tenants_TenantId",
+                table: "Payments",
+                column: "TenantId",
+                principalTable: "Tenants",
+                principalColumn: "Id");
+        }
+    }
+}
