c7a0b7490e2cb35ddbd30d48f033298cd1f32dd0
eniikeoluwa.olaniyi <eniikeoluwa.olaniyi@clearwox.com>
2025-10-09 20:24:52 +0100

    gaps schedule implementation

---
+++ src/Nova.API/Application/Actions/GapsSchedule/Queries/GetGapsScheduleByIdQuery.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using FluentResults;
+using FluentValidation;
+using Nova.Contracts.Models;
+using Nova.Domain.Utils;
+using Nova.API.Application.Services.Data;
+
+namespace Nova.API.Application.Actions.GapsSchedule.Queries;
+
+public record GetGapsScheduleByIdQuery(string Id) : IRequest<Result<GapsScheduleResponse>>;
+
+public class GetGapsScheduleByIdHandler : IRequestHandler<GetGapsScheduleByIdQuery, Result<GapsScheduleResponse>>
+{
+    private readonly IGapsScheduleService _gapsScheduleService;
+
+    public GetGapsScheduleByIdHandler(IGapsScheduleService gapsScheduleService)
+    {
+        _gapsScheduleService = gapsScheduleService;
+    }
+
+    public async Task<Result<GapsScheduleResponse>> Handle(GetGapsScheduleByIdQuery query, CancellationToken cancellationToken)
+    {
+        return await _gapsScheduleService.GetGapsScheduleByIdAsync(query.Id);
+    }
+}
