f7c235506908dc323b3f887f468ebd33c1cc17cb
Olaniyi Eniikeoluwa <110841397+Eniikeoluwa@users.noreply.github.com>
2025-12-28 18:06:17 +0100

    Update Auth.cs

--- src/Nova.Contracts/Models/Auth.cs
+++ src/Nova.Contracts/Models/Auth.cs
@@ -6,7 +6,7 @@ public class SignupRequest
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string TenantId { get; set; } = string.Empty; // User selects from available tenants
+    public string TenantId { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
 }
 
